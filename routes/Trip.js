17e6e77ed59c448dc9c6bd74e081c6c07b582f1f
bandisaiteja534 <bandisaiteja.57@gmail.com>
2024-09-26 00:26:49 +0530

    first commit

--- routes/Trip.js
+++ routes/Trip.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const Trip = require('../models/Trip');
-const Trip = require('../models/Trip');
 
 router.get('/', async (req, res) => {
     try {
